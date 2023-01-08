1056878f50bcb2e25aab51b618e87ef5e251552b
Tyrics <2395234635@qq.com>
2023-01-08 15:31:53 +0800

    feat: simple header nav

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
 <template>
+  <NavBar />
   <RouterView />
 </template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const navList = computed(() => [
+  { title: 'nav1', link: '/' },
+  { title: 'nav2', link: '/' },
+  { title: 'nav3', link: '/' },
+])
+</script>
+
+<template>
+  <header flex items-center max-w-screen-2xl mx-auto h16 px-8>
+    <section>
+      <RouterLink v-slot="{ navigate }" to="/" custom>
+        <h1 cursor-pointer font-italic text-violet @click="navigate()">
+          Passerby Spring
+        </h1>
+      </RouterLink>
+    </section>
+    <section ml-auto>
+      <nav space-x-8>
+        <RouterLink v-for="item of navList" :key="item.title" text-xl text-gray-7 border-b hover:border-b-solid :to="item.link">
+          {{ item.title }}
+        </RouterLink>
+      </nav>
+    </section>
+  </header>
+</template>
